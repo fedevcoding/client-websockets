63a9e558873d21fddc6acf93ca718d63d1db8173
Federico <federicocavallini100@gmail.com>
2023-08-29 09:48:34 +0200

    fix type errors

--- types/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { ReconnectingWebSocket, PollingWebsocket, Web3Websocket } from "src";
-
-declare module "ts-websockets" {
-  export { ReconnectingWebSocket, PollingWebsocket, Web3Websocket };
-}
