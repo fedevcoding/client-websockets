94cc37142e00cc62cc308aa099197cc2fb4930da
Federico <federicocavallini100@gmail.com>
2023-10-03 18:43:31 +0200

    add viem block websocket

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import ReconnectingWebSocket from "./Reconnectingwebsocket";
 import PollingWebsocket from "./PollingWebsocket";
 import Web3Websocket from "./Web3Websocket";
+import ViemBlockWebsocket from "./ViemBlockWebsocket";
 
-export { ReconnectingWebSocket, PollingWebsocket, Web3Websocket };
+export {
+  ReconnectingWebSocket,
+  PollingWebsocket,
+  Web3Websocket,
+  ViemBlockWebsocket,
+};
