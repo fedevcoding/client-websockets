4f4d50da6291445b66d87c4f454f9761a70193ed
Federico <federicocavallini100@gmail.com>
2023-12-29 10:08:08 +0100

    refactor package

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,5 @@
 import ReconnectingWebSocket from "./Reconnectingwebsocket";
 import PollingWebsocket from "./PollingWebsocket";
 import Web3Websocket from "./Web3Websocket";
-import ViemBlockWebsocket from "./ViemBlockWebsocket";
 
-export {
-  ReconnectingWebSocket,
-  PollingWebsocket,
-  Web3Websocket,
-  ViemBlockWebsocket,
-};
+export { ReconnectingWebSocket, PollingWebsocket, Web3Websocket };
